eeb5a4c2525996ee3999f1c1ea1d18df2b7b72ff
Iswathi Priya <iswathichidambaram@gamil.com>
2024-11-09 20:30:04 +0530

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">
+          <h3>Contact no</h3>
+            <p>+91 93549 65478</p>
+            <h3>Email</h3>
+            <p>Saasrestaurant@gamil.com</p>
+            
+          </div>
+          <div className="right">
+            <h3>Address</h3>
+            <p>Saas Restaurant, chennai</p>
+         
+            <h3>Timing</h3>
+            <p>Open: 08:00 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+          
+          </div>
+          <div className="right">
+            <p>Copyright 2024 Saas restaurant.All rights reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
